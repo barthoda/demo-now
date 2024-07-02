ee4ca2e815525732649ccde1718d9bc8bfedd600
barthoda <barthoda@users.noreply.github.com>
2024-07-02 10:45:36 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - live update for Jason and Gary
         </a>
       </header>
     </div>
